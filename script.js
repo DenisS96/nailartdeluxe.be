6ba47b264ee35454b04a5db9e58f0e4ee3b2c7ce
DenisS96 <88380857+DenisS96@users.noreply.github.com>
2022-10-07 12:41:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ myID = document.getElementById("myID");
 
 var myScrollFunc = function () {
   var y = window.scrollY;
-  if (y >= 700) {
+  if (y >= 500) {
     myID.className = "prijslijst show";
   } else {
     myID.className = "prijslijst hide";
