53f029f7c94ec3febb0359d1a2499095d2923581
DenisS96 <88380857+DenisS96@users.noreply.github.com>
2022-10-14 07:06:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ $(".navicon").on("click", function (e) {
   $(".toggle").toggleClass("toggle--active");
 });
 
+$(".toggle__menu--item").click(function () {
+  $(".navicon").click();
+});
 myID = document.getElementById("myID");
 
 var myScrollFunc = function () {
