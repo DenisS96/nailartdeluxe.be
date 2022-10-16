1f6e40a4dfdec12a1520020ba4a89d09c5c08165
DenisS96 <88380857+DenisS96@users.noreply.github.com>
2022-10-16 08:11:48 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(".navicon").on("click", function (e) {
+  e.preventDefault();
+  $(this).toggleClass("navicon--active");
+  $(".toggle").toggleClass("toggle--active");
+});
+
+$(".toggle__menu--item").click(function () {
+  $(".navicon").click();
+});
+myID = document.getElementById("myID");
+
+var myScrollFunc = function () {
+  var y = window.scrollY;
+  if (y >= 600) {
+    myID.className = "prijslijst show";
+  } else {
+    myID.className = "prijslijst hide";
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
